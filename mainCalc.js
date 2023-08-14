d1b18de97b85664b4b779893dc576ae50dc8169e
Achmad Damanhuri <139878625+damar009@users.noreply.github.com>
2023-08-15 01:27:48 +0700

    main file

---
+++ mainCalc.js
@@ -0,0 +1,53 @@
+
+import { performSqrt } from './sqrt.js';
+import { performExit } from './exit.js';
+
+function displayMenu() {
+    return parseInt(
+        prompt(`Apa yang ingin Anda lakukan?\n
+        1. ADDITION - Leon
+        2. SUBSTRACTION - raisa
+        3. DIVISION - dimas Yusuf
+        4. MULTIPLICATION - Andika
+        5. SQUARE - Jusuf Fahri
+        6. SQRT - Achmad
+        7. CBRT - Daniel
+        8. EXIT - Keluar
+    `));
+}
+
+let menu;
+
+do {
+    menu = displayMenu();
+
+    switch (menu) {
+        case 1:
+            performAddition()
+            break;
+        case 2:
+            performSubtraction()
+            break;
+        case 3:
+            performDivision()
+            break;
+        case 4:
+            performMultiplication()
+            break;
+        case 5:
+            performSquare()
+            break;
+        case 6:
+            performSqrt()
+            break;
+        case 7:
+            performCbrt()
+            break;
+        case 8:
+            performExit()
+            break;
+        default:
+            alert("Pilihan tidak valid. Coba lagi.");
+            break;
+    }
+} while (menu !== 8);
