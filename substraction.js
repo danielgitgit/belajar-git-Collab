af382b411ce8cbb2681be65d9cc3be7c35c49b3f
rsdrni <139878195+rsdrni@users.noreply.github.com>
2023-08-15 20:34:58 +0700

    update feature-substraction

--- substraction.js
+++ substraction.js
@@ -1,4 +1,6 @@
-export function performSubtraction(num1, num2){
+export function performSubtraction(){
+    let num1 = parseInt(prompt('Masukkan angka pertama: '));
+    let num2 = parseInt(prompt('Mau dikurangi dengan: '));
     let result = num1 - num2
     alert(`Hasil dari ${num1} dikurang ${num2} adalah ${result}`)
 }
