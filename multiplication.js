95ebd44108ff123062ed71ad40ff369097bd15db
hikmareta <andhika1499@gmail.com>
2023-08-15 17:39:05 +0700

    chore: updated code in multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -1,4 +1,4 @@
-export function multiplication(num1, num2) {
+export function performMultiplication(num1, num2) {
   let result = num1 * num2;
   alert(`The result of ${num1} multiplied by ${num2} is ${result}`);
 }
