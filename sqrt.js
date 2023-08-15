9e7ea40cb024992bb40dd612516bc564376e98e2
rsdrni <139878195+rsdrni@users.noreply.github.com>
2023-08-15 16:34:29 +0700

    Delete sqrt.js

--- sqrt.js
+++
@@ -1,5 +0,0 @@
-export function performSqrt() {
-    let number = parseInt(prompt('Masukkan angka/bilangan yang akan dihitung akar pangkat duanya!'));
-    let result = Math.sqrt(number);
-    alert(`Hasil dari sqrt(${number}) adalah ${result}`);
-}
